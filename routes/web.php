b9278bc050c6e2a97c3e0e46c9e8c2e5b78acffd
nadznaf <148078917+nadznaf@users.noreply.github.com>
2024-09-08 23:43:27 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
+
+Route::get('categories', [CategoryController::class, 'index']);
+
+Route::get('/routes', [PostController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('posts', [PostController::class, 'index']);
-use App\Http\Controllers\PostController;
