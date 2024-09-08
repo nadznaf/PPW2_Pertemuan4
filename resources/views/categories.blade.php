8b50e16be0b03113ac88df94bb23f5ac7e87e343
nadznaf <148078917+nadznaf@users.noreply.github.com>
2024-09-08 23:42:20 +0700

    Add files via upload

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Category</title>
+</head>
+<body>
+    <h1>Category</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Categories</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Title 1</td>
+                <td>Category 1</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Title 2</td>
+                <td>Category 2</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Title 3</td>
+                <td>Category 3</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
